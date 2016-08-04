7b95ca0da5eb4a6452e1b76407bcb5bc50c76652
rianday <rianday@rianday.green>
2016-08-04 17:40:45 +0700

    ok

--- resources/views/prod/realisasi.blade.php
+++ resources/views/prod/realisasi.blade.php
@@ -41,24 +41,36 @@
                                             <!-- <th>No Pnwr</th> -->
                                             <th style="min-width:200px;">Customer</th>
                                             <th style="min-width:250px;">Deskripsi</th>
-                                            <!-- <th style="min-width:100px;">Tanggal</th> -->
-                                            <!-- <th>00</th><th>01</th><th>02</th><th>03</th><th>04</th><th>05</th><th>06</th><th>07</th><th>08</th><th>09</th><th>10</th><th>11</th> -->
-                                            <!-- <th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>19</th><th>20</th><th>21</th><th>22</th><th>23</th> -->
                                             <!-- <th>05</th><th>07</th><th>08</th><th>09</th><th>10</th><th>11</th><th>12</th><th>13</th><th>14</th> -->
-                                            <tr ng-repeat="values2 in _jam" class="jam">
-                                              [[ ($index < 10) ? '0'.$index : $index ]]  
-                                            </tr>
+                                            <th ng-repeat="values2 in _jam" class="jam">
+                                              [[ (($index+5) < 10) ? '0'+($index+5) : ($index+5) ]]  
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr ng-repeat="values in _data">
+                                        <tr ng-repeat="values in _data" ng-init="sectionIndex = $index">
                                             <!-- <td>[[ values.f_pnwr ]]</td> -->
                                             <td>[[ values.pnwr.customer.nama_customer ]]</td>
                                             <td>[[ values.pnwr.judul_iklan ]]</td>
                                             <!-- <td>[[ values.tayang_tgl ]]</td> -->
-                                            <td ng-repeat="values2 in _jam" class="jam">
-                                                <input ng-model="dataModal[$parent.$index].tayang_jam[$index]" ng-if="((values.jam).indexOf(values2+5)>0)" class="form-control input-sm" mask='59' mask-clean='true' restrict="reject">
-
+                                            <td ng-repeat="values3 in _jam" class="jam">
+                                                <!-- <input ng-model="dataModal[$parent.$index].tayang_jam[$index]" ng-if="((values.jam[$parent.$index]).indexOf(values2+5)>-1)" class="form-control input-sm" mask='59' mask-clean='true' restrict="reject"> -->
+                                                <!-- <input ng-model="dataModal[$parent.$index].tayang_jam[$index]" ng-if="((values.jam[$parent.$index]).indexOf(values2+5)>-1)" class="form-control input-sm" mask='59' mask-clean='true' restrict="reject"> -->
+                                                <!-- <span ng-repeat="values.jam in _frekuensi"> -->
+                                                    <!-- <span ng-if="angular.isArray([values.jam[$parent.$index]])"> ok</span> -->
+                                                    <!-- [[ angular.isArray([]) ]]  -->
+                                                    <!-- [[ angular.isString(values.jam[$parent.$index]) ]]  -->
+                                                <span ng-if="((values.jam[$parent.$index]).indexOf(values3+5)>-1)">
+                                                    <input ng-model="<tayang_realisasi sectionIndex=""></tayang_realisasi>[$index]" ng-change="save(tayang_realisasi, $parent.$index)" ng-if="((values.jam[$parent.$index]).indexOf(values3+5)>-1)" class="form-control input-sm"><!-- mask='59' mask-clean='true' restrict="reject">-->
+                                                </span>
+                                                <!-- </span> -->
+                                                <!--
+                                                [[ items.splice(index, 1) ]]
+                                                [[ values.jam[$parent.$index]+ ' #$ ' + sectionIndex +'/'+ (values3+5) + '#' + $parent.$index + '\\' + (values.jam[$parent.$index]).indexOf(values3+5) ]]   
+                                                -->
+                                                <!-- [[ values.jam[$parent.$index].forEach(function(i) { count[i] = (count[i]||0)+1;  }); ]]  -->
+                                                <!-- [[ ((values.jam[$parent.$index]).indexOf(values2+5)>0) ]]     -->
+                                                <!-- [[ (jQuery.inArray(values2+5, values.jam[$parent.$index])>-1) ]]     -->
                                                 <!-- [[ ((values.jam).indexOf(values2)>0) ? 'ada' : '' ]] -->
                                             </td>
                                         </tr>
@@ -67,7 +79,8 @@
                             </div>
                             <div class="pull-right">
                                 <button type="button" class="btn btn-default" ng-click="toggle('cancel',null)">Cancel</button>
-                                <button type="button" class="btn btn-primary" id="btn-save" ng-click="save(modalstate, idState)" ng-disabled="frmMst.$invalid">Save changes</button>
+                                <button type="button" class="btn btn-primary" id="btn-save" ng-click="save(tayang_realisasi)" ng-disabled="frmMst.$invalid">Save changes</button>
+                                <!-- <button type="button" class="btn btn-primary" id="btn-save" ng-click="save(_data, _tanggal)" ng-disabled="frmMst.$invalid">Save changes</button> -->
                                 <!-- <p class="pull-left pagination">Menampilan <strong>[[ table.CurrentItems ]]</strong> Dari <strong>[[ table.TotalItems ]]</strong></p> -->
                                 <!-- <dir-pagination-controls class="pull-right" on-page-change="pageChanged(newPageNumber)" template-url="/ext/ng-html/dirPagination.html" ></dir-pagination-controls> -->
                             </div>
